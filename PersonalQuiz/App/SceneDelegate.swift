164f10cff0301ab4bac6882f3292c266a0b2b3fd
Denis Vasilev <lyon_den@mail.ru>
2022-12-01 14:08:48 +0300

    Initial refactoring

---
+++ PersonalQuiz/App/SceneDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  SceneDelegate.swift
+//  PersonalQuiz
+//
+//  Created by Денис Васильев on 01.12.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
