ec3313c8dfdab0c9bd785886af2577cc309e7e94
Denis Vasilev <lyon_den@mail.ru>
2022-12-06 17:03:21 +0300

    work with logic

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -15,18 +15,33 @@ class ResultViewController: UIViewController {
     // MARK: - Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
+        test()
     }
 
     // MARK: - IB Actions
     @IBAction func doneButtonPressed(_ sender: UIBarButtonItem) {
         navigationController?.dismiss(animated: true)
     }
+    
+    // MARK: - Private Methods
+    private func test() {
+        var chosenAnimal: [Animal] = []
+        for chosen in answersChosen {
+            print(chosen.animal)
+            chosenAnimal.append(chosen.animal)
+        }
+        
+        var counts: [Animal: Int] = [:]
+        
+        for animal in chosenAnimal {
+            counts[animal] = (counts[animal] ?? 0) + 1
+        }
+        
+        print(counts)
+    }
 }
 
-// MARK: - Private Methods
-extension ResultViewController {
-    
-}
+
 
 //[PersonalQuiz.Answer(title: "Стейк", animal: PersonalQuiz.Animal.dog),
 // PersonalQuiz.Answer(title: "Плавать", animal: PersonalQuiz.Animal.dog),
